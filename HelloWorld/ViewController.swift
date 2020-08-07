12a256368b8acc684a9222fbb653eeb3025cdf7d
shigasy <kxx555u.u@gmail.com>
2020-08-07 22:36:16 +0900

    add: storyboradを使って，labelとかbuttonとか．buttonにイベント追加

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,12 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var helloLabel: NSTextField!
+    
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+            name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
